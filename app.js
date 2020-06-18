8d315afb37747ebdbdfea305bc4a9c0cc57e96fb
AnonymousTechEd <64619639+AnonymousTechEd@users.noreply.github.com>
2020-06-18 07:57:03 -0400

    Finished Week 2 Assingment
    I finished the week 2 assingment.

--- app.js
+++ app.js
@@ -1,34 +1,91 @@
-(function () {
-    "use strict";
-    
-    angular
-        .module("App", [])
-        .controller("ApController", appController)
-        .filter("Replace", replaceFilter)
-        
-        appController.$inject = ["$scope", "$filter", "ReplaceFilter"];
+(function() {
+    'use strict';
 
+    angular.module('ShoppingListApp', [])
+        .controller('ShoppingListAddController', ShoppingListAddController)
+        .controller('ShoppingListShowController', ShoppingListShowController)
+        .controller('ShoppingListShowController2', ShoppingListShowController2)
+        .service('ShoppingListService', ShoppingListService);
 
+    ShoppingListAddController.$inject = ['ShoppingListService'];
 
-        function appController($scope, $filter, ReplaceFilter) {
-            $scope.inputvalue = "";
-            $scope.array = ["Avocado", "Apple", "Cake Batter", "Bannana", "Pinapple","Pencil","Pen","Crayon"]
-            $scope.search = ""
-            $scope.InputButton = function () {
-                $scope.search =  $scope.inputvalue
-            }
+    function ShoppingListAddController(ShoppingListService) {
+        var itemAdder = this;
+
+        itemAdder.itemName = "";
+        itemAdder.itemQuantity = "";
+
+        itemAdder.addItem = function() {
+            ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
         }
+    }
 
-        function replaceFilter() {
-            return function (input, a, b) {
-                input = input || "";
-                input = input.replace(a, b)
-                return input
+
+    ShoppingListShowController.$inject = ['ShoppingListService'];
+
+    function ShoppingListShowController(ShoppingListService) {
+        var showList = this;
+
+        showList.items = ShoppingListService.getItems(1);
+
+        showList.removeItem = function(itemIndex) {
+            ShoppingListService.removeItem(itemIndex, 1);
+        };
+        showList.BuyItem = function(itemIndex) {
+            ShoppingListService.BuyItem(itemIndex);
+        };
+    }
+    ShoppingListShowController2.$inject = ['ShoppingListService'];
+
+    function ShoppingListShowController2(ShoppingListService) {
+        var showList2 = this;
+
+        showList2.items = ShoppingListService.getItems();
+
+        showList2.removeItem = function(itemIndex) {
+            ShoppingListService.removeItem(itemIndex);
+        };
+    }
+
+
+
+
+
+
+    function ShoppingListService() {
+        var service = this;
+
+        // List of shopping items
+        var items = [];
+        var items2 = [];
+
+        service.addItem = function(itemName, quantity) {
+            var item = {
+                name: itemName,
+                quantity: quantity
+            };
+            items.push(item);
+        };
+
+        service.removeItem = function(itemIndex, which) {
+            if (which == 1) {
+                items.splice(itemIndex, 1);
+                } else {
+                    items2.splice(itemIndex, 1);
+                }
+        };
+        service.BuyItem = function(itemIndex) {
+            var item = items[itemIndex]
+            items.splice(itemIndex, 1);
+            items2.push(item)
+        };
+        service.getItems = function(which) {
+            if (which == 1) {
+            return items;
+            } else {
+                return items2
             }
-        }
-
-
-
-
+        };
+    }
 
 })();
